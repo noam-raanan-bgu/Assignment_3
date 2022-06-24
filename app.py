70360f0359a6177b56f6999b3a479ab0e542d941
noam-raanan-bgu <raananno@post.bgu.ac.il>
2022-06-24 23:47:18 +0300

    Part C

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask ,redirect ,url_for
-from  flask import render_template
+from flask import Flask, redirect, url_for, request
+from flask import render_template, session
 app = Flask(__name__)
+app.secret_key = '123'
 
+#Part B
 
 @app.route('/')
 def cv_main_page():
@@ -23,6 +25,48 @@ def assignment3_page():
 def header_page():
     return render_template('header.html')
 
+#Part C
+users = {
+    'NOAM': {'name': 'noam', 'email': 'noamraanan55@gmail.com'},
+    'TAMAR': {'name': 'tamar', 'email': 'tamar7595@gmail.com'},
+    'GIDEON': {'name': 'gideon', 'email': 'graanan@gmail.com'},
+    'SARAH': {'name': 'sarah', 'email': 'sarahra@gmail.com'},
+    'CHUBBY': {'name': 'chubby', 'email': 'chubbychub@gmail.com'}
+}
+
+@app.route('/assignment3_2', methods=['GET','POST'])
+def search_3_2():
+
+
+    if request.method == 'GET':
+        if 'name' in request.args and 'email' in request.args and 'is_submit' in request.args:
+            name = request.args['name']
+            email = request.args['email']
+            is_submit = request.args['is_submit']
+
+
+            for value in users.values():
+                if(value.get("name") == name and value.get("email") == email):
+                    return render_template('assignment3_2.html', name=name, email=email)
+
+
+            if(is_submit):
+                return render_template('assignment3_2.html', users=users)
+        return render_template('assignment3_2.html')
+
+
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        if 'username' in request.form:
+            session['username'] = username
+            return render_template('assignment3_2.html', username=username)
+
+
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return render_template('assignment3_2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
